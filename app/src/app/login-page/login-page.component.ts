ed7f8c52c3d62a935221f79c09ac824175c8b169
Jarod Aerts <t-jaaert@microsoft.com>
2019-08-14 15:08:08 -0700

    Added thing to allow redirect to work for pr build

--- app/src/app/login-page/login-page.component.ts
+++ app/src/app/login-page/login-page.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"]
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    var linkElement = document.getElementById("link") as HTMLLinkElement;
+    if (linkElement !== null && linkElement !== undefined) {
+      linkElement.href =
+        "https://authentication.azurestaticwebsites.net/.auth/login/google?post_login_redirect_uri=" +
+        window.location.href.split("://")[1] + "list";
+    }
   }
-
 }
